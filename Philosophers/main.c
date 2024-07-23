d1f6524ab109195a402ca01ecda6d2feb45caf6a
Hamdi Warren <warren.hamdi@gmail.com>
2024-07-23 16:54:00 +0200

    [WIP]

--- Philosophers/main.c
+++ Philosophers/main.c
@@ -6,12 +6,26 @@
 /*   By: whamdi <whamdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 13:10:52 by whamdi            #+#    #+#             */
-/*   Updated: 2024/07/22 14:32:25 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2024/07/23 16:51:17 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_lib.h"
-#include <stdio.h>
+
+void ft_issleeping(t_data *data, int i)
+{
+	long chrono =  (data->time_sleep - data->ms) / 10;
+	while(1)
+	{
+		pthread_mutex_lock(&data->write_mutex);
+		ft_time(data);
+		printf("%ld %d is sleeping[😴]\n",data->ms, i);
+		pthread_mutex_unlock(&data->write_mutex);
+		if(data->ms > chrono)
+			break;
+	}
+
+}
 
 void ft_eat(t_data *data,int i)
 {
@@ -25,11 +39,11 @@ void ft_eat(t_data *data,int i)
 	{
 		pthread_mutex_lock(&data->write_mutex);
 		ft_time(data);
-		printf("%ld %d is eating[🍝]\n",data->ms, i);
-		pthread_mutex_unlock(&data->write_mutex);
 		if(data->ms > chrono)
 			break;
 	}
+	printf("%ld %d is eating[🍝]\n",data->ms, i);
+	pthread_mutex_unlock(&data->write_mutex);
 }
 
 bool ft_takefork(t_data *data, int i)
@@ -47,15 +61,24 @@ bool ft_takefork(t_data *data, int i)
 	return(true);
 }
 
-void ft_isdying(t_data *data, int i)
+void ft_isdying(t_data *data) 
 {
-	ft_time(data);
-	int chrono = data->time_die - data->ms;
-	if(chrono < 0)
-		chrono = chrono * -1;
-	if(data->ms > data->time_die)
-		printf("%ld %d died\n",data->ms,i);
+    int i = 0;
+    while (i < data->philo_nbr) 
+	{
+        ft_time(data);
+        long time_since_last_meal = data->ms - data->philos[i].last_meal;
+        if (time_since_last_meal > data->time_die) 
+		{
+            pthread_mutex_lock(&data->write_mutex);
+            printf("%ld %d died\n", data->ms, data->philos[i].id);
+            pthread_mutex_unlock(&data->write_mutex);
+            exit(EXIT_FAILURE);
+        }
+        i++;
+    }
 }
+
 void *ft_routine(void *arg)
 {
 	t_philo *philo = (t_philo *)arg;
@@ -68,13 +91,16 @@ void *ft_routine(void *arg)
         }
 
 	if(ft_takefork(data, i) == true)
+	{
 		ft_eat(data,i);
+		ft_issleeping(data, i);
+	}
 	pthread_mutex_unlock(&data->philos[i % data->philo_nbr].fork);
 	pthread_mutex_unlock(&data->philos[(i + 1) % data->philo_nbr].fork);
 	pthread_mutex_lock(&data->write_mutex);
 	printf("Philosopher is thinking[🤔]...\n");
 	ft_time(data);
-	ft_isdying(data, i);
+	ft_isdying(data);
 	pthread_mutex_unlock(&data->write_mutex);
 	pthread_mutex_destroy(&data->write_mutex);
     return NULL;
@@ -90,7 +116,8 @@ int start_simulation(t_data *data)
     if (!data->philos)
         return (-1);
 
-    while (i < data->philo_nbr)
+    // init mutex must create a func
+	while (i < data->philo_nbr)
     {
         data->philos[i].id  = i+1;
 		data->philos[i].data = data;
@@ -102,7 +129,8 @@ int start_simulation(t_data *data)
 		i++;
     }
 	i = 0;
-    while (i < data->philo_nbr)
+    // create thread with instruction in ft_routibe for every threads
+	while (i < data->philo_nbr)
     {
         ft_time(data);
 		if (pthread_create(&philo_threads[i], NULL, ft_routine, (void *)&data->philos[i]) != 0)
