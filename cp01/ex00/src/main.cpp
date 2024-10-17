f6e5ac5b2c697eb75c9528df4e5590ff8943a135
Hamdi Warren <warren.hamdi@gmail.com>
2024-10-17 16:38:16 +0200

    [ex01] : polish

--- cp01/ex00/src/main.cpp
+++ cp01/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: whamdi <whamdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 11:21:26 by whamdi            #+#    #+#             */
-/*   Updated: 2024/10/11 10:55:30 by whamdi           ###   ########.fr       */
+/*   Updated: 2024/10/17 16:37:34 by whamdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(void)
 {
 	Zombie *z;
 	randomChump("Servietsky");	
-	z = newZombie("YO");
+	z = newZombie("Generic Zombie");
 	std::cout << z->name << std::endl;
 	delete z;
 	return(0);
