f51563a2f11f6bd526c59cba6b41e9a0910ece3f
Hamdi Warren <warren.hamdi@gmail.com>
2024-12-08 13:25:37 +0100

    00 done

--- cp07/ex00/src/main.cpp
+++ cp07/ex00/src/main.cpp
@@ -1,42 +1,5 @@
 #include <iostream>
-
-template <class T> void swap(T &x, T &y)
-{
-	T tmp;
-	tmp = x;
-	x = y;
-	y = tmp;
-}
-template <class T> T min(T x, T y)
-{
-	if(x < y)
-	{
-		return(x);
-	}
-	else if(x == y)
-	{
-		return(y);
-	}
-	else
-	{
-		return(y);
-	}
-}
-template <class T> T max(T x, T y)
-{
-	if(x > y)
-	{
-		return(x);
-	}
-	else if(x == y)
-	{
-		return(y);
-	}
-	else
-	{
-		return(y);
-	}
-}
+#include "../includes/temp.hpp"
 
 int main( void ) 
 {
