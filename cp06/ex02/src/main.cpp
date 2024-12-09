e0db0c3cf86fde7e28a38a7c7a240b6e40e31d7c
Warren Hamdi <whamdi@paul-f4Br6s2.clusters.42paris.fr>
2024-12-09 10:43:24 +0100

    cpp06 done

--- cp06/ex02/src/main.cpp
+++ cp06/ex02/src/main.cpp
@@ -1,13 +1,24 @@
 #include "../includes/Base.hpp"
 
 int main() {
-    Base* obj = generate();
     
-    identify(obj);
-
-    identify(*obj);
-
-    delete obj;
-
+	size_t value = 0;
+	std::cout << "Press 1 to continue generate Base class or 2 to exit" << std::endl;		
+	std::cin >> value;  
+	while(1)
+	{
+		if(value == 1)
+		{
+			Base* obj = generate();
+			identify(obj);
+			identify(*obj);
+			delete obj;
+			value = 0;
+			std::cout << "Press 1 to continue generate Base class or 2 to exit" << std::endl;
+			std::cin >> value;
+			if(value == 2)
+				return(1);
+		}
+	}
     return 0;
 }
